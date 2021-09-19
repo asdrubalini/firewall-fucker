9a1852f3625513f8fcfea381f764f5a4d635b462
Giovanni <indirizzoemail@mail.com>
2021-09-19 15:51:29 +0200

    finalize

--- start-tunnel.sh
+++ start-tunnel.sh
@@ -4,6 +4,5 @@
 WIREGUARD_IP=`getent hosts wireguard | awk '{ print $1 }'`
 
 # Start server
-# ./wstunnel -v --udp --server wss://0.0.0.0:443 -r wireguard:51820
 ./wstunnel -v --server wss://0.0.0.0:443 -r $WIREGUARD_IP:51820
 
