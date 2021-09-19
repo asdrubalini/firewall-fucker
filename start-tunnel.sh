e09150ff41e92511595231e50196da48c890cb94
Giovanni <indirizzoemail@mail.com>
2021-09-19 23:22:04 +0200

    print ip

--- start-tunnel.sh
+++ start-tunnel.sh
@@ -3,6 +3,8 @@
 # Resolve wireguard's container IP
 WIREGUARD_IP=`getent hosts wireguard | awk '{ print $1 }'`
 
+echo $WIREGUARD_IP
+
 # Start server
 ./wstunnel -v --server wss://0.0.0.0:443 -r $WIREGUARD_IP:51820
 
